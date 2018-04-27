9c8a80018eff859baabaa5fd94aa0d386bd75ff9
Zoltán Gyebrovszki <kavics@sensenet.com>
2018-04-27 09:47:08 +0200

    Increment version numbers to v7.1.1.

--- src/Workflow/Properties/AssemblyInfo.cs
+++ src/Workflow/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 #if DEBUG
@@ -8,16 +7,16 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SenseNet.Workflow (Release)")]
 #endif
 
-[assembly: AssemblyDescription("Workflow component for the sensenet ECM platform.")]
+[assembly: AssemblyDescription("Workflow component for the sensenet platform.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Sense/Net Inc.")]
 [assembly: AssemblyCopyright("Copyright © Sense/Net Inc.")]
-[assembly: AssemblyProduct("sensenet ECM")]
+[assembly: AssemblyProduct("sensenet")]
 [assembly: AssemblyTrademark("Sense/Net Inc.")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("7.1.0.0")]
-[assembly: AssemblyFileVersion("7.1.0.0")]
-[assembly: AssemblyInformationalVersion("7.1.0")]
+[assembly: AssemblyVersion("7.1.1.0")]
+[assembly: AssemblyFileVersion("7.1.1.0")]
+[assembly: AssemblyInformationalVersion("7.1.1")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("6b47b471-efdf-4f7b-82b7-bcea425d2a78")]
