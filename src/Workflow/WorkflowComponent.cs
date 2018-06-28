8fd4c6b8d1200b4256377b29d718f98f309a52eb
tusmester <tusmester@gmail.com>
2018-06-28 17:39:15 +0200

    Add Supported component version info to workflow component.

--- src/Workflow/WorkflowComponent.cs
+++ src/Workflow/WorkflowComponent.cs
@@ -1,9 +1,11 @@
-﻿using SenseNet.ContentRepository;
+﻿using System;
+using SenseNet.ContentRepository;
 
 namespace SenseNet.Workflow
 {
     public class WorkflowComponent : SnComponent
     {
         public override string ComponentId => "SenseNet.Workflow";
+        public override Version SupportedVersion { get; } = new Version(7, 1, 0);
     }
 }
